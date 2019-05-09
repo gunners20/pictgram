653c52bc607c6ebdaec7c8edb638b907fbeb008f
池田朋矢 <omiyafc3.optimusg@docomo.ne.jp>
2019-05-09 12:30:16 +0900

    rails 6

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+                   length: { maximum: 15 }
+
+  validates :email, presence: true,
+                    format: { with: /\w+@\w+\.\w+/}
+
+  validates :password, presence: true,
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
+  has_secure_password
+end
