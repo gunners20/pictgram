523258709cdf3822a885261d59579c7e5d2b0619
池田朋矢 <omiyafc3.optimusg@docomo.ne.jp>
2019-05-10 15:41:50 +0900

    rails6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(session_params)
-    if user &&user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email])
+    if user &&user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -30,5 +30,5 @@ class SessionsController < ApplicationController
 
   def session_params
     params.require(:session).permit(:email, :password)
-  end  
+  end
 end
